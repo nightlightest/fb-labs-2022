aac84f1aaf9080f9f84429bf959905233273b79d
nightlightest <83123170+nightlightest@users.noreply.github.com>
2022-11-09 13:02:29 +0200

    semi-working

--- cp_3/rzhevskii_fb-03_borschevskii_fb-03/main.py
+++ cp_3/rzhevskii_fb-03_borschevskii_fb-03/main.py
@@ -1,13 +1,17 @@
-import re
 import itertools
-import math
-import csv
 from itertools import *
+import math
+# import sys
+# sys.setrecursionlimit(10000)
 
 alletters1 = ['а', 'б', 'в', 'г', 'д', 'е', 'ж', 'з', 'и', 'й',
               'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф',
               'х', 'ц', 'ч', 'ш', 'щ', 'ы', 'ь', 'э', 'ю', 'я']
 popular_bigrams = ['ст', 'но', 'то', 'на', 'ен']
+forbiddenbigrams = {#'аь', 'аы', 'еы', 'еь', 'оь', 'оы',
+                    'уь', 'уы', 'эы', 'эь', 'яь', 'яы', 'йь', 'йы',
+                    'ыы', 'ьь'}
+# forbiddenbigrams = {'аъ', 'аь', 'аы'}
 
 
 def countletterfrequency(text):
@@ -35,7 +39,7 @@ def countletterfrequency(text):
     return percentages
 
 
-def countbigrams(text):
+def countbigramsfull(text):
     alletters = alletters1
     step = 2
     # text = open("refactoredtext.txt").read()
@@ -63,29 +67,64 @@ def countbigrams(text):
         pct = round((v / totaloccurences), 5)
         # print(k, str(pct))
         # percentages.append(pct)
-        percentages.append(k)
+        if v > 0:
+            percentages.append(k)
+    # print(percentages[:5])
+    return percentages
+
+
+def countbigrams(text):
+    alletters = alletters1
+    allbigrams = []
+    templist = [alletters, alletters]
+    for element in itertools.product(*templist):
+        allbigrams.append(''.join(element))
+
+    bigramfrequency = dict.fromkeys(allbigrams, 0)
+    for i in range(0, len(text) - 1, 2):
+        bigramfrequency[str(text[i]) + str(text[i + 1])] += 1
+
+    # totaloccurences = sum(bigramfrequency.values())
+    percentages = []
+    for k, v in dict(reversed(sorted(bigramfrequency.items(), key=lambda item: item[1]))).items():
+        # pct = round((v / totaloccurences), 5)
+        # print(k, str(pct))
+        # percentages.append(pct)
+        if v > 0:
+            percentages.append(k)
     # print(percentages[:5])
     return percentages[:5]
 
 
-def gcdextended(a, b):
-    if a == 0:
-        return b, 0, 1
-    gcd, x1, y1 = gcdextended(b % a, a)
-    x = y1 - (b // a) * x1
-    y = x1
-    return gcd, x, y
+# def gcdextended(a, b):
+#     if a == 0:
+#         return b, 0, 1
+#     gcd, x1, y1 = gcdextended(b % a, a)
+#     x = y1 - (b // a) * x1
+#     y = x1
+#     return gcd, x, y
+
+def gcdextended(b: int, n: int):
+    (x0, x1, y0, y1) = (1, 0, 0, 1)
+    while n != 0:
+        (q, b, n) = (b // n, n, b % n)
+        (x0, x1) = (x1, x0 - q * x1)
+        (y0, y1) = (y1, y0 - q * y1)
+    return b, x0, y0
 
 
 def linearmodulequation(a, b, n):
     gcd, x, y = gcdextended(a, n)
+    # gcd = math.gcd(a, n)
     if gcd == 1:
         return (x * b) % n
     else:
+        return "Розв'язків немає"
         if b % gcd != 0:
             return 'Розв`язків немає'
         else:
             gcd_b, x_b, y_b = gcdextended(b, n)
+            # gcd_b = math.gcd(b, n)
             result = linearmodulequation(a / gcd_b, b / gcd_b, n / gcd_b)
             solution = []
             while result < n:
@@ -113,7 +152,7 @@ def hit_index_count(text):
     return hit_index
 
 
-def convert_bigram_to_number(bigram_list):
+def convert_bigramlist_to_number(bigram_list):
     alletters = alletters1
     # result = alletters.index(bigram[0]) * len(alletters) + alletters.index(bigram[1])
     result = []
@@ -122,13 +161,27 @@ def convert_bigram_to_number(bigram_list):
     return result
 
 
-def convert_number_to_bigram(number_list):
+def convert_bigram_to_number(singlebigram):
+    alletters = alletters1
+    result = alletters.index(singlebigram[0]) * len(alletters) + alletters.index(singlebigram[1])
+    return result
+
+
+def convert_numberlist_to_bigram(number_list):
     alletters = alletters1
     result = []
     for i in number_list:
         letter_1 = alletters[i // 31]
         letter_2 = alletters[i % 31]
         result.append(letter_1 + letter_2)
+    return result
+
+
+def convert_number_to_bigram(number):
+    alletters = alletters1
+    letter_1 = alletters[number // 31]
+    letter_2 = alletters[number % 31]
+    result = letter_1 + letter_2
     return result
 
 
@@ -149,29 +202,49 @@ def find_key(x1, x2, y1, y2):
     return key
 
 
+def decryption2(text, key):
+    bigram_list_enc = []
+    number_list_dec = []
+    for i in range(0, len(text) - 1, 2):
+        bigram_list_enc.append(text[i] + text[i + 1])
+    number_list_enc = convert_bigramlist_to_number(bigram_list_enc)
+    if type(key[0]) != list:
+        for i in number_list_enc:
+            number_list_dec.append(linearmodulequation(key[0], (i - key[1]) % 31 ** 2, 31 ** 2))
+    else:
+        for t in range(len(key)[0]):
+            decryption(text, [key[0][t], key[1][t]])
+    bigram_list_dec = convert_numberlist_to_bigram(number_list_dec)
+    dec_text = "".join(bigram_list_dec)
+    return dec_text
+
+
 def decryption(text, key):
     bigram_list_enc = []
     number_list_dec = []
     for i in range(0, len(text) - 1, 2):
         bigram_list_enc.append(text[i] + text[i + 1])
-    number_list_enc = convert_bigram_to_number(bigram_list_enc)
+    number_list_enc = convert_bigramlist_to_number(bigram_list_enc)
     for i in number_list_enc:
-        number_list_dec.append(linearmodulequation(key[0], (i - key[1]) % 31 ** 2, 31 ** 2))
-    bigram_list_dec = convert_number_to_bigram(number_list_dec)
+        u = linearmodulequation(key[0], (i - key[1]) % 31 ** 2, 31 ** 2)
+        if isinstance(u, str):
+            return '0'
+        number_list_dec.append(u)
+    bigram_list_dec = convert_numberlist_to_bigram(number_list_dec)
     dec_text = "".join(bigram_list_dec)
     return dec_text
 
 
 def key_pairs(text_bigrams):
     popular_bigrams = ['ст', 'но', 'то', 'на', 'ен']
-    a = convert_bigram_to_number(popular_bigrams)
-    b = convert_bigram_to_number(text_bigrams)
+    a = convert_bigramlist_to_number(popular_bigrams)
+    b = convert_bigramlist_to_number(text_bigrams)
     pairs = [list(item) for item in product(a, b)]
     all_keys = [list(item) for item in product(pairs, pairs)]
     return all_keys
 
 
-def key_guessing(sourcetext):
+def key_guessing1(sourcetext):
     key_guess_list = []
     for i in key_pairs(countbigrams(sourcetext)):
         key_guess_list.append(find_key(i[0][0], i[0][1], i[1][0], i[1][1]))
@@ -180,33 +253,77 @@ def key_guessing(sourcetext):
     return key_guess_list
 
 
-def key_validating(sourcetext):
-    valid_key_list = []
-    for i in key_guessing(sourcetext):
-        text_index = hit_index_count(decryption(sourcetext, i))
-        # print(text_index)
-        if 0.045 <= text_index <= 0.059:
-            valid_key_list.append(i)
-    for i in valid_key_list:
-        print(decryption(sourcetext, i))
-    return valid_key_list
+def key_guessing(sourcetext):
+    popularbigramsintext = countbigrams(sourcetext)
+    popularbigramsintext = convert_bigramlist_to_number(popularbigramsintext)
+    popular_bigrams_num = convert_bigramlist_to_number(popular_bigrams)
+    for i in popularbigramsintext:
+        for j in popularbigramsintext:
+            if i == j:
+                continue
+            for k in popular_bigrams_num:
+                for m in popular_bigrams_num:
+                    if k == m:
+                        continue
+                    # print(i, k, j, m)
+
+                    suggestedkey = find_key(k, m, i, j)
+                    # print(suggestedkey)
+                    # if suggestedkey == [713, 191]:
+                    #     abc = 'mylife'
+                    #     pass
+
+                    if isinstance(suggestedkey[0], str):
+                        continue
+                    if key_validating(sourcetext, suggestedkey):
+                        pass
+                        # return suggestedkey
+
+
+def key_validating(sourcetext, key):
+    # valid_key_list = []
+    # for i in key_guessing(sourcetext):
+    #     text_index = hit_index_count(decryption(sourcetext, i))
+    #     # print(text_index)
+    #     if 0.045 <= text_index <= 0.059:
+    #         valid_key_list.append(i)
+    # for i in valid_key_list:
+    #     print(decryption(sourcetext, i))
+    decrypted = decryption2(sourcetext, key)
+    # print(decrypted[:100])
+    if decrypted == '0':
+        return False
+    # print(decrypted[:100])
+    allbigrams = countbigramsfull(decrypted)
+    for i in allbigrams:
+        if i in forbiddenbigrams:
+            # print(i)
+            return False
+    print(key)
+    print(decrypted[:100])
+    return True
 
 
 # print(gcdExtended(2, 16))
 # print(LinearModulEquation(2, 3, 7))
-sourcetext = open("V4", encoding='utf-8').read()
+sourcetext = open("16.txt", encoding='utf-8').read()
+sourcetext = sourcetext.replace('\n', '')
 # print(sourcetext)
-print(countbigrams(sourcetext))
-print(convert_bigram_to_number(countbigrams(sourcetext)))
-print(popular_bigrams)
-print(convert_bigram_to_number(popular_bigrams))
-print(find_key(545, 802, 417, 162))
-print(convert_number_to_bigram([0, 222, 111, 444, 555, 666, 777, 888]))
-print(decryption(sourcetext, [788, 524]))
+
+# print(countbigrams(sourcetext))
+# print(convert_bigramlist_to_number(countbigrams(sourcetext)))
+# print(popular_bigrams)
+# print(convert_bigramlist_to_number(popular_bigrams))
+# print(find_key(545, 802, 417, 162))
+# print(convert_numberlist_to_bigram([0, 222, 111, 444, 555, 666, 777, 888]))
+# print(decryption(sourcetext, [788, 524]))
 # a = convert_bigram_to_number(popular_bigrams)
 # b = convert_bigram_to_number(countbigrams(sourcetext))
 # pair = [list(item) for item in product(a, b)]
 # print(pair)
 # print(*[(a, b) for a in A for b in B])
 # print(list(product(A, B)))
-print(key_validating(sourcetext))
+
+key1 = key_guessing(sourcetext)
+# text1 = decryption(sourcetext, [390, 10])
+print(key1)
